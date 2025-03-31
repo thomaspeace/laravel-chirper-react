8f5e1995c892c1825be471f4d761ac9af4ae50c0
thomaspeace <thomasjpeace29@gmail.com>
2025-03-31 23:31:05 +0100

    add comments for the Chirps/Index.jsx React component

--- resources/js/Pages/Chirps/Index.jsx
+++ resources/js/Pages/Chirps/Index.jsx
@@ -5,12 +5,19 @@ import PrimaryButton from '@/Components/PrimaryButton';
 import { useForm, Head } from '@inertiajs/react';
  
 export default function Index({ auth }) {
+
+    // data stores the data, currently just message
+    // post = POST request to the backend
+    // processing = boolean indicating if form is being submitted
+    // reset = reset form after successful submission
+    // errors = contains any validation errors from the backend
     const { data, setData, post, processing, reset, errors } = useForm({
         message: '',
     });
  
     const submit = (e) => {
         e.preventDefault();
+        // route() is an Inertia helper that generates the URL route from the backend
         post(route('chirps.store'), { onSuccess: () => reset() });
     };
  
@@ -24,6 +31,7 @@ export default function Index({ auth }) {
                         value={data.message}
                         placeholder="What's on your mind?"
                         className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                        // setData is a fn provided by Inertia's useForm
                         onChange={e => setData('message', e.target.value)}
                     ></textarea>
                     <InputError message={errors.message} className="mt-2" />
