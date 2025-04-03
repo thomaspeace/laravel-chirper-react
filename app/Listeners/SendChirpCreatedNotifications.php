dcb96c927616f649a0dab127c0f2b548701e61db
thomaspeace <thomasjpeace29@gmail.com>
2025-04-03 18:29:13 +0100

    update SendChirpCreatedNotifications.php to listen for the ChirpCreated event ina a queue

--- app/Listeners/SendChirpCreatedNotifications.php
+++ app/Listeners/SendChirpCreatedNotifications.php
@@ -3,10 +3,14 @@
 namespace App\Listeners;
 
 use App\Events\ChirpCreated;
+use App\Models\User;
+use App\Notifications\NewChirp;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
-class SendChirpCreatedNotifications
+// ShouldQueue tell Laravel that the listener should be run in a queue
+// To run queued job, run "php artisan queue:work" in terminal
+class SendChirpCreatedNotifications implements ShouldQueue
 {
     /**
      * Create the event listener.
@@ -21,6 +25,9 @@ class SendChirpCreatedNotifications
      */
     public function handle(ChirpCreated $event): void
     {
-        //
+        // Send notifications to every user in the platform, except the author of the Chirp
+        foreach (User::whereNot('id', $event->chirp->user_id)->cursor() as $user) {
+            $user->notify(new NewChirp($event->chirp));
+        }
     }
 }
